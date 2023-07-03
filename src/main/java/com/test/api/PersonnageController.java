9d16679c4a81b266a9afb9eb63c7aa091c24b9c7
Nicolas Nieloux <nicolas.nieloux@le-campus-numerique.fr>
2023-07-03 09:50:33 +0200

    Ajout de updatePersonnage

--- src/main/java/com/test/api/PersonnageController.java
+++ src/main/java/com/test/api/PersonnageController.java
@@ -1,6 +1,10 @@
 package com.test.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
@@ -9,9 +13,10 @@ public class PersonnageController {
 
     private final PersonnageDao personnageDao;
 
-    public PersonnageController(PersonnageDao personnageDao){
+    public PersonnageController(PersonnageDao personnageDao) {
         this.personnageDao = personnageDao;
     }
+
     private int idCount;
 
     @GetMapping("/personnage")
@@ -20,60 +25,50 @@ public class PersonnageController {
         Iterable<Personnage> personnages = personnageDao.findAll();
         return personnages;
     }
+
+    @PostMapping("/personnage")
+
+    public ResponseEntity<Void> ajouterPersonnage(
+            @RequestBody Personnage personnage) {
+
+        Personnage personnageAdd = personnageDao.save(personnage);
+        if (personnageAdd == null)
+            return ResponseEntity.noContent().build();
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(personnageAdd.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
 //
-//    @PostMapping("/personnage")
-//    @Operation(
-//            description = "Ajout d'un personnage",
-//            responses = {
-//                    @ApiResponse(
-//                            responseCode = "200",
-//                            description = "personnage crée!",
-//                            content = @Content(
-//                                    mediaType = "application/json"
-//                            )
-//                    )
-//            }
-//    )
-//
-//    public Personnage ajouterPersonnage(
-//             @RequestBody Personnage personnage) {
-//
-//        String name = personnage.getName();
-//        Type type= personnage.getType();
-//
-//
-//        Personnage personnageNew = new Personnage(this.idCount, name, type, 10);
-//        personnages.add(personnageNew);
-//        this.idCount++;
-//
-//        return personnageNew;
-//    }
-////
 
     @GetMapping("/personnage/{id}")
-       public Personnage findPersonnage(@PathVariable("id") int id) {
-            return personnageDao.findById(id);
+    public Personnage findPersonnage(@PathVariable("id") int id) {
+        return personnageDao.findById(id);
 
     }
 
-//    @PutMapping("/personnage/{id}")
-//      public Personnage updatePersonnage(@PathVariable("id") int id,
-//                                                       @RequestBody Personnage personnageToUpdate) {
-//
-//        Personnage personnage = personnages.stream().filter(person -> person.getId() == id).findFirst().orElse(null);
-//
-//            personnage.setName(personnageToUpdate.getName());
-//            personnage.setType(personnageToUpdate.getType());
-//            personnage.setLifePoint(personnageToUpdate.getLifePoint());
-//
-//            return personnage;
-//
-//}
-//
+
     @DeleteMapping("/personnage/{id}")
     public void deletePersonnage(@PathVariable("id") int id) {
 
         personnageDao.deleteById(id);
+    }
+
+    @PutMapping("/personnage/{id}")
+    public Personnage updatePersonnage(@PathVariable("id") int id,
+                                       @RequestBody Personnage personnageToUpdate) {
+
+        Personnage personnage = personnageDao.findById(id);
+
+        personnage.setName(personnageToUpdate.getName());
+        personnage.setType(personnageToUpdate.getType());
+        personnage.setLifePoint(personnageToUpdate.getLifePoint());
+
+        return personnageDao.save(personnage);
 
     }
+
 }
